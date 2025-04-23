71620dae48027c427cbc1c45b05c29da60d31afd
yog27ray <yog27ray@gmail.com>
2025-04-23 17:40:03 +0530

    fix: self declaration of the repository.

--- src/annotation/repository.ts
+++ src/annotation/repository.ts
@@ -7,10 +7,10 @@ import { processClassConfig } from '../common/class-config';
 
 export function repository<
   T extends new() => Repository
->(BindTo: ServiceIdentifier<Repository>, config: { dependency?: Array<new () => Dependency<InstanceType<T>>>; } = {}): (Target: T) => void {
+>(BindTo?: ServiceIdentifier<Repository>, config: { dependency?: Array<new () => Dependency<InstanceType<T>>>; } = {}): (Target: T) => void {
   return function decorator(Target: T): void {
     const _config = processClassConfig(config);
-    loadInContainer(getContainer('repository'), Target, { isSingleton: true }, BindTo);
+    loadInContainer(getContainer('repository'), Target, { isSingleton: true }, BindTo || Target);
     Object.assign(Target, { classId: _config.classId });
   };
 }
